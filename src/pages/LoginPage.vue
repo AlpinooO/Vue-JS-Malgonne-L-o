67025ea8ce52a3484b481fd930c34a282fd506dd
AlpinooO <alpino.cdc@gmail.com>
2025-02-12 18:09:59 +0100

    refactor LoginPage to use refs for email and password, implement input validation utility

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -5,7 +5,7 @@
             <section>
                 <article>
                     <label for="email"></label>
-                    <input v-model="data.email"
+                    <input v-model="email"
                     id="email"
                     placeholder="Entrez votre email"
                     type="email"
@@ -16,7 +16,7 @@
             <section>
                 <article>
                     <label for="password"></label>
-                    <input v-model="data.password"
+                    <input v-model="password"
                     id="password"
                     placeholder="Entrez votre mot de passe"
                     type="password"
@@ -34,50 +34,42 @@
 </template>
 
 <script lang="ts" setup>
-import { watch, reactive} from "vue";
+import { watch, reactive, ref} from "vue";
+import InputValidator from '../utils/input-validator.ts';
 
-const data = reactive({
-    email: '',
-    password: ''
+// const data = reactive({
+//     email: '',
+//     password: ''
+// })
+
+const email = ref('')
+const password = ref('')
+
+watch(email, (val) =>{
+    console.log(val,InputValidator(val, 'email'));
 })
 
-watch(data, (val) =>{
-    console.log(val.email, isUserInputValid(val.email));
-    console.log(val.password, isUserPasswordValid(val.password));
+watch(password,(val) =>{
+    console.log(val,InputValidator(val, 'password'));
 })
 
+// const isUserPasswordValid = (input:string): boolean => {
+//     const pattern1 = new RegExp(/^(?=.*[A-Z] *[a-z])(?=.*\d)(?=.*[@#$:%*?&])[A-Za-z\d @#$:%*?&]{8,}$/);
+//     return pattern1.test(input)
+// }
+// if (isUserPasswordValid("1234Test*")){
+//     console.log('Mot de passe valide');
+// } else {
+//     console.log('Mot de passe invalide');
+// }
 
-const isUserInputValid = (input:string): boolean => {
-    const pattern = new RegExp(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/);
-    return pattern.test(input)
 
-
-
-}
-if (isUserInputValid("test@test.com")){
-    console.log('Email valide');
-} else {
-    console.log('Email invalide');
-}
-
-const isUserPasswordValid = (input:string): boolean => {
-    const pattern1 = new RegExp(/^(?=.*[A-Z] *[a-z])(?=.*\d)(?=.*[@#$:%*?&])[A-Za-z\d @#$:%*?&]{8,}$/);
-    return pattern1.test(input)
-}
-if (isUserPasswordValid("1234Test*")){
-    console.log('Mot de passe valide');
-} else {
-    console.log('Mot de passe invalide');
+const submitHandler = async()=> {
+    const result = await fetch('users.json');
+    const users = await result.json();
+    console.log(users);
 }
 
-
-const submitHandler = ()=> {
-
-}
-
-const inputHandler = function(){
-
-}
 </script>
 
 <style lang="scss" scoped>
